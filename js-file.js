65ea79c3d6d9b41fb169eb2e679370755a2a17fc
Sebastian <enghseb@gmail.com>
2022-07-06 16:00:40 +0000

    Add basic arithmetic operations and operator to handle input

---
+++ js-file.js
@@ -0,0 +1,27 @@
+function operator(num, num2, operator){
+    answer = operator(num, num2)
+    return answer
+};
+    
+function add(num, num2) {
+    sum = num+num2
+    return sum
+};
+
+function subtract(num, num2){
+    difference = num-num2
+    return difference
+};
+
+function divide(num, num2){
+    quotient = num/num2
+    return quotient
+};  
+
+function multiply(num, num2) {
+    factor = num*num2
+    return factor
+};
+
+operator(5,2,subtract)
+console.log(answer)
